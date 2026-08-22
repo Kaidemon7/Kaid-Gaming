b0f7d953c414f4a264214eaf8cf499058e473bd8
Kaidemon7 <kaidemon7@users.noreply.github.com>
2026-08-21 20:16:15 -0500

    Add /js/main.js stub (233 game refs), adinLoader.js + cloak.js stubs

---
+++ js/main.js
@@ -0,0 +1,5 @@
+﻿// Stub: /js/main.js (3kh0 compatibility)
+// Games that call main() define their own override in subsequent scripts
+if (typeof window.main === "undefined") {
+  window.main = function() { console.log("main stub called"); };
+}
